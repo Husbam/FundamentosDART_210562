088c30ba6ca8b348e1eb57b6f9af7970bfbffe6b
Jose Angel Gomez Ortiz <210562@utxicotepec.edu.mx>
2024-10-04 09:31:11 -0600

    Pracvtica 11
    Creacion del  archivo de la practica 11

--- DartPad/Ext9_ConstructoresNombrados.dart
+++ DartPad/Ext9_ConstructoresNombrados.dart
@@ -1,29 +1,25 @@
+void main() {
+    final mySquare = Square(side:18);
+    mySquare._side = -32;
 
-void main(){
-   final mySquare = Square(side:18);
-   mySquare._side = -32;
-   
-   print("""Lado del cuadrado: ${mySquare._side}
-   Área que ocupa: ${mySquare.calculateArea()}""");
+    print("""Lado del cuadrado: ${mySquare._side}
+          Área que ocupa: ${mySquare.calculateArea()}""");
 }
 
 class Square {
     double _side;
     Square({required double side})
     :_side=side;
-    
-    double get area
-    {
+
+    double get area {
         return _side*_side;
     }
-    set side(double value)
-    {
+    set side(double value) {
         print("Asignando un nuevo valor al lado: $value");
         if(value <0) throw "EL valor debe ser mayor a 0";
         _side=value;
     }
-    double calculateArea()
-    {
+    double calculateArea() {
         return _side*_side;
     }
 }
