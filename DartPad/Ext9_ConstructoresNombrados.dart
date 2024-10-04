ef65bb55b43b30a38bed44e1c9e7535cdb3f4e85
Jose Angel Gomez Ortiz <210562@utxicotepec.edu.mx>
2024-10-04 10:11:14 -0600

    Pracvtica 11
    Actualizacion de la Practica

--- DartPad/Ext9_ConstructoresNombrados.dart
+++ DartPad/Ext9_ConstructoresNombrados.dart
@@ -1,25 +1,40 @@
 void main() {
-    final mySquare = Square(side:18);
-    mySquare._side = -32;
+    final mySquare = Square(side: 18);
 
-    print("""Lado del cuadrado: ${mySquare._side}
+    print("""
+          Lado del Cuadrado: ${mySquare.side}
           Área que ocupa: ${mySquare.calculateArea()}""");
-}
 
+    mySquare.side = -32;
+
+}
 class Square {
     double _side;
+
+
     Square({required double side})
-    :_side=side;
+    : _side = side;
+
 
     double get area {
-        return _side*_side;
+        return _side * _side;
     }
+
+    double get side {
+        return _side;
+    }
+
+
     set side(double value) {
         print("Asignando un nuevo valor al lado: $value");
-        if(value <0) throw "EL valor debe ser mayor a 0";
-        _side=value;
+
+        if (value < 0) throw 'El valor del lado debe ser mayor a 0.';
+
+        _side = value;
     }
+
+
     double calculateArea() {
-        return _side*_side;
+        return _side * _side;
     }
 }
